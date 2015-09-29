1c91cd70acb46c42ad40ebe61eee982502fd6665
ha132604 <woodmetal98@gmail.com>
2015-09-29 19:58:16 +0900

    ㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ

--- JavaPractice/src/org/dimigo/collection/MelonChart.java
+++ JavaPractice/src/org/dimigo/collection/MelonChart.java
@@ -3,6 +3,7 @@
  */
 package org.dimigo.collection;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -24,18 +25,17 @@ public class MelonChart {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		List<Music> list=new LinkedList();
+		List<Music> list=new ArrayList();
 		list.add(new Music("바람이나 좀 쐐","개리"));
 		list.add(new Music("보통연애","박경"));
 		list.add(new Music("취향저격","iKon"));
 		System.out.println("-- << 멜론 차트 >> --");
         printList(list);
         System.out.println("-- << 2위 곡 추가 >> --");
-        list.add(new Music("레옹", "이유갓지"));
+        list.add(1,new Music("레옹", "이유갓지"));
         printList(list);
         System.out.println("-- << 3위 곡 변경 >> --");
-        list.get(2).setTitle("맙소사");
-        list.get(2).setSinger("황태지");
+        list.set(2,new Music("맙소사","황태지"));
         printList(list);
         System.out.println("-- << 4위 곡 삭제 >> --");
         list.remove(3);
